79e032358d6d3ec12a59be195b2d00c95fe92dc8
taehyun <ttu510@naver.com>
2023-10-23 16:50:15 +0900

    jenkins test

--- src/test/java/com/example/projectfinal/ProjectFinalApplicationTests.java
+++ src/test/java/com/example/projectfinal/ProjectFinalApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.projectfinal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectFinalApplicationTests.class, properties = "spring.main.allow-bean-definition-overriding=true")
 class ProjectFinalApplicationTests {
 
     @Test
